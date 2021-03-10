5cc4bd995e6e647b1745073d4b01dea9826700b0
Revelation2106 <55440018+Revelation2106@users.noreply.github.com>
2021-03-10 21:30:01 +0000

    Updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,36 +1,30 @@
 project "ImGui"
-	kind "StaticLib"
-	language "C++"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++17"
+    staticruntime "on"
+    
+    targetdir ("bin/" .. outputDir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputDir .. "/%{prj.name}")
 
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+    
+    filter "system:windows"
+        systemversion "latest"
 
-	files
-	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter "system:linux"
-		pic "On"
-		systemversion "latest"
-		cppdialect "C++17"
-		staticruntime "On"
-
-	filter "configurations:Debug"
+    filter "configurations:Debug"
 		runtime "Debug"
 		symbols "on"
 
